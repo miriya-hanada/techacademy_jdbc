00dab8635aac4a1d3dd066b56a5726a5984b2da6
miriya-hanada <56401488+miriya-hanada@users.noreply.github.com>
2020-02-26 10:52:03 +0900

    Update DbConnectSample02.java

--- src/dbSample/DbConnectSample02.java
+++ src/dbSample/DbConnectSample02.java
@@ -9,39 +9,48 @@ import java.sql.Statement;
 public class DbConnectSample02 {
 
     public static void main(String[] args) {
-
+           // データベース接続と結果取得のための変数
         Connection con = null;
         Statement stmt = null;
         ResultSet rs = null;
 
         try {
+            // 1. ドライバのクラスをJava上で読み込む
             Class.forName("com.mysql.jdbc.Driver");
 
+            // 2. DBと接続する
             con = DriverManager.getConnection(
                     "jdbc:mysql://localhost/world?useSSL=false",
                     "root",
                     "smile0220");
-
+            
+            // 3. DBとやりとりする窓口（Statementオブジェクト）の作成
             stmt = con.createStatement();
 
+            // 4, 5. Select文の実行と結果を格納／代入
             String name = "' or 'a' = 'a";
             rs = stmt.executeQuery("select * from country where Name = '" + name + "'");
 
+            // 6. 結果を表示する
             while (rs.next()) {
                 System.out.println(rs.getString("Name"));
                 System.out.println(rs.getInt("Population"));
             }
 
+             // 6-1. データの更新を行う
             int count = stmt.executeUpdate("update country set Population = 105000 where Code = 'ABW'");
             System.out.println(count);
 
         } catch (SQLException e) {
+             // DBとの処理で何らかのエラーがあった場合の例外
             e.printStackTrace();
 
         } catch (ClassNotFoundException e) {
+            // JDBCドライバを読み込めないエラーがあった場合の例外
             e.printStackTrace();
 
         } finally {
+            // 7. 接続を閉じる
             if (rs != null) {
                 try {
                     rs.close();
